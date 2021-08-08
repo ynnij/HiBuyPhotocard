250703c8127e3dab97e6fb05fa818ae45f066837
Orange <ghdwn643@naver.com>
2021-08-08 16:02:00 +0900

    채팅창_2번째

---
+++ app/src/main/java/com/example/hibuyphotocard/Fragment1Adpater.java
@@ -0,0 +1,17 @@
+package com.example.hibuyphotocard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fragment1Adpater extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message2);
+
+
+    }
+}
