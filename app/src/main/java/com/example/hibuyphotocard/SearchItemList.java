d7c3f6f0e6214492f74d6f0000bb9ca27cd14f3a
hyejin <rla07gowls10@ewhain.net>
2021-08-09 13:47:53 +0900

    chore: 필요 데이터 관련 Getter & Setter 구현 및 intent로 값을 넘겨주기 위한 Serializable 추가

--- app/src/main/java/com/example/hibuyphotocard/SearchItemList.java
+++ app/src/main/java/com/example/hibuyphotocard/SearchItemList.java
@@ -1,24 +1,47 @@
 package com.example.hibuyphotocard;
 
-public class SearchItemList {
+import java.io.Serializable;
 
-  //  private String price;
-
+public class SearchItemList implements Serializable {
     private long price;
-
     private String userName;
     private String imageURI;
+    private String groupTag;
+    private String albumTag;
+    private String memberTag;
+    private String sellID;
+
+    public String getSellID() {
+        return sellID;
+    }
+
+    public void setSellID(String sellID) {
+        this.sellID = sellID;
+    }
 
     public SearchItemList(){}
+    public String getGroupTag() {
+        return groupTag;
+    }
 
-//    public String getPrice() {
-//        return price;
-//    }
+    public void setGroupTag(String groupTag) {
+        this.groupTag = groupTag;
+    }
 
-//    public void setPrice(String price) {
-//        this.price = price.toString();
-//    }
+    public String getAlbumTag() {
+        return albumTag;
+    }
 
+    public void setAlbumTag(String albumTag) {
+        this.albumTag = albumTag;
+    }
+    public String getMemberTag() {
+        return memberTag;
+    }
+
+    public void setMemberTag(String memberTag) {
+        this.memberTag = memberTag;
+    }
     public long getPrice() {
         return price;
     }
