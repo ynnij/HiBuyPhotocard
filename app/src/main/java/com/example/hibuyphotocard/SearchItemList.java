e4002d7df10657ed00ccf7e6d95dba092e7fb668
hyejin <rla07gowls10@ewhain.net>
2021-08-25 23:32:07 +0900

    feat: 각 판매글 클릭 시 sellPageActivity로 값 넘기기 및 adapter 새로고침

--- app/src/main/java/com/example/hibuyphotocard/SearchItemList.java
+++ app/src/main/java/com/example/hibuyphotocard/SearchItemList.java
@@ -3,14 +3,39 @@ package com.example.hibuyphotocard;
 import java.io.Serializable;
 
 public class SearchItemList implements Serializable {
-    private long price;
+    private String price;
     private String userName;
     private String imageURI;
     private String groupTag;
     private String albumTag;
     private String memberTag;
     private String sellID;
+    private String delivery;
+    private String detail;
+    private String state;
 
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(String delivery) {
+        this.delivery = delivery;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
     public String getSellID() {
         return sellID;
     }
@@ -31,22 +56,21 @@ public class SearchItemList implements Serializable {
     public String getAlbumTag() {
         return albumTag;
     }
-
     public void setAlbumTag(String albumTag) {
         this.albumTag = albumTag;
     }
+
     public String getMemberTag() {
         return memberTag;
     }
-
     public void setMemberTag(String memberTag) {
         this.memberTag = memberTag;
     }
-    public long getPrice() {
+
+    public String getPrice() {
         return price;
     }
-
-    public void setPrice(long price) {
+    public void setPrice(String price) {
         this.price = price;
     }
 
